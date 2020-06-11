9beb7606bc371a203475a6c7781c7ba3167c3b10
PapaKramb <guccigang5554@gmail.com>
2020-06-11 21:44:52 +0900

    Show latest post

--- app.rb
+++ app.rb
@@ -36,6 +36,10 @@ configure do
 end
 
 get '/' do
+	# выбираем список постов из БД
+
+	@results = @db.execute 'select * from Posts order by id desc'
+
 	erb :index		
 end
 
@@ -43,10 +47,6 @@ end
 # (браузер получает страницу с сервера)
 
 get '/new' do 
-	# выбираем список постов из БД
-
-	@results = @db.execute 'select * from Posts order by id desc'
-
 	erb :new
 end
 
